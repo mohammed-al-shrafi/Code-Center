e21f2d916c86650745deeac4e6b4e6a7aa76b294
mohammed-al-shrafi <m14.alshrafi7@gmail.com>
2025-12-03 22:42:55 +0200

    Refactore & Oraganize

---
+++ js/elements.js
@@ -0,0 +1,15 @@
+export const ul = document.querySelector('.nav-bar__links');
+export const linksTags = document.querySelectorAll('.nav-bar__links li a');
+export const hamburger = document.querySelector('.hamburger');
+export const courseType = document.querySelectorAll('.type');
+
+export const modal = document.getElementById('modal');
+export const cancelBtn = document.querySelector('.cancel-btn');
+export const closeModalBtn = document.querySelector('.close-modal-btn');
+export const submitBtn = document.querySelector('.submit-btn');
+export const bookCoursebtn = document.querySelector('.book-course');
+
+export const user = document.querySelector('[data-user]');
+export const tel = document.querySelector('[data-tel]');
+export const courseSelect = modal.querySelector('#course');
+export const buyBtns = document.querySelectorAll('.buy');
